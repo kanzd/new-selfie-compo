739d0033591f3bcddb372cfd28c0dec46d2aa572
Kanishk Dixit <kanishk.dixit@zestmoney.in>
2022-04-22 09:57:27 +0530

    cdscs

--- src/Shared/Video/index.tsx
+++ src/Shared/Video/index.tsx
@@ -35,7 +35,7 @@ export default function Index(Props: any) {
         <div className='video-camera'>
             
             <div className='inside'>
-                <video className='user-video' src="" ></video>
+                <video id='user-video' src="" ></video>
             </div>
             <div className='frame'>
                 <div className='top'>
@@ -57,7 +57,17 @@ export default function Index(Props: any) {
 
             </div>
         </div>
-        <div className='button'>
+        <div className='button' onClick={(e)=>{
+            const video:HTMLVideoElement | any = document.querySelector("#user-video");
+            const canvas:HTMLCanvasElement | any = document.createElement("canvas");
+            canvas.height=video.videoHeight;
+            canvas.width=video.videoWidth;
+            const ctx = canvas.getContext('2d');
+            ctx.drawImage(video, 0, 0);
+            const dataUri = canvas.toDataURL(`image/png`);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            console.log(dataUri);
+        }}>
 
         </div>
         </>
